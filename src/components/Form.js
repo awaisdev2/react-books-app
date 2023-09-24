0a212660e51be3fd7afd0ec74e9323bc38d70229
Awais <awaisdev2@gmail.com>
2023-09-24 14:20:56 +0500

    complete app

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,195 +2,195 @@
 import React from "react";
 
 function Form() {
+  let [books, setBooks] = React.useState([]);
+  let [myNewBook, setMyNewBook] = React.useState({});
+  let [isEdit, setIsEdit] = React.useState(false);
+  let [editBooksObj, setEditBooksObj]  = React.useState({});
+  let [bookName, setBookName] = React.useState("");
+  let [authorName, setAuthorName] = React.useState("");
+  let [status, setStatus] = React.useState("");
+  let [publishDate, setPublishDate] = React.useState("");
+  let [bookPages, setBookPages] = React.useState("");
+  let [bookLanguage, setBookLanguage] = React.useState("");
 
-  let books = []
-  let myNewBook = {}
-  let isEdit = false
-  let editBooksObj = {}
-
-  let onSubmit = () => {
-    const bookName = document.getElementById("bookName").value;
-    const authorName = document.getElementById("authorName").value;
-    const status = document.getElementById("status").value;
-    const publishDate = document.getElementById("publishDate").value;
-    const bookPages = document.getElementById("pages").value;
-    const bookLanguage = document.getElementById("bookLanguage").value;
-    if (
-      bookName === "" ||
-      authorName === "" ||
-      status === "" ||
-      bookLanguage === "" ||
-      publishDate === "" ||
-      bookPages === ""
-    ) {
-      return (document.getElementById("booksList").innerHTML =
-        "Please fill the empty fields");
-    }
-    if (
-      bookName.length < 2 ||
-      authorName.length < 2 ||
-      status.length < 2 ||
-      publishDate.length < 4 ||
-      bookPages.length < 2 ||
-      bookLanguage.length < 3
-    ) {
-      return (document.getElementById("booksList").innerHTML =
-        "Each field must require atleast 2 or 4 character");
-    } else {
-      const data = [bookName, authorName, status, bookLanguage, publishDate];
-      document.getElementById("booksList").innerHTML = data;
-    }
-    myNewBook = {
+  const onSubmit = () => {
+    const newBook = {
       name: bookName,
       author: authorName,
-      bookStatus: status,
-      date: publishDate,
-      bookPages: bookPages,
+      status: status,
+      publishDate: publishDate,
+      pages: bookPages,
       language: bookLanguage,
     };
+  
     if (isEdit) {
-      const index = books.findIndex((item) => item.name === editBooksObj.name);
-      if (index !== -1) {
-        books[index] = myNewBook;
-      }
-      isEdit = false;
+      const updatedBooks = books.map((book) =>
+        book.name === editBooksObj.name ? newBook : book
+      );
+      setBooks(updatedBooks);
     } else {
-      books.push(myNewBook);
+      setBooks((prevBooks) => [...prevBooks, newBook]);
     }
-    showInfo(books);
+  
     clearFields();
   };
+  
+  
 
   const clearFields = () => {
-    document.getElementById("bookName").value = "";
-    document.getElementById("authorName").value = "";
-    document.getElementById("status").value = "";
-    document.getElementById("publishDate").value = "";
-    document.getElementById("pages").value = "";
-    document.getElementById("bookLanguage").value = "";
+    setBookName("");
+    setAuthorName("");
+    setStatus("");
+    setPublishDate("");
+    setBookPages("");
+    setBookLanguage("");
+    setIsEdit(false); // Reset isEdit
+    setEditBooksObj({});
   };
 
-  const showInfo = (booksList) => {
-    document.getElementById("booksList").innerHTML = "";
-    let bookList = "";
-    window.books = booksList;
-    booksList.forEach((book, index) => {
-      bookList = bookList + "<h1>" + book.name + "</h1>";
-      bookList = bookList + "<p>" + book.author + "</p>";
-      bookList = bookList + "<p>" + book.bookStatus + "</p>";
-      bookList = bookList + "<p>" + book.date + "</p>";
-      bookList = bookList + "<p>" + book.bookPages + "</p>";
-      bookList = bookList + "<p>" + book.language + "</p>";
-     });
-    document.getElementById("booksList").innerHTML = bookList;
+  // const searchBook = () => {
+  //   const searchInput = document.getElementById("searchBook").value;
+  //   let bookList = "";
+  //   const filterbooks = books.filter((book) => {
+  //     return book.name === searchInput;
+  //   });
+  //   if (searchInput === "") {
+  //     return (document.getElementById("booksList").innerHTML = bookList);
+  //   }
+  // };
+
+  const deleteBook = (i) => {
+    const updatedBooks = books.filter((_, index) => index !== i);
+    setBooks(updatedBooks);
   };
 
-  const searchBook = () => {
-    const searchInput = document.getElementById("searchBook").value;
-    let bookList = "";
-    const booksList = books;
-    booksList.forEach((book, index) => {
-      bookList = bookList + "<h1>" + book.name + "</h1>";
-      bookList = bookList + "<p>" + book.author + "</p>";
-      bookList = bookList + "<p>" + book.bookStatus + "</p>";
-      bookList = bookList + "<p>" + book.date + "</p>";
-      bookList = bookList + "<p>" + book.bookPages + "</p>";
-      bookList = bookList + "<p>" + book.language + "</p>";
-     });
-    const filterbooks = books.filter((book) => {
-      return book.name === searchInput;
-    });
-    if (searchInput === "") {
-      return (document.getElementById("booksList").innerHTML = bookList);
-    }
-    showInfo(filterbooks);
+  const editBook = (book) => {
+    setIsEdit(true);
+    setEditBooksObj(book); // Store the book being edited
+    setBookName(book.name);
+    setAuthorName(book.author);
+    setStatus(book.status);
+    setBookPages(book.pages);
+    setBookLanguage(book.language);
+    setPublishDate(book.publishDate);
   };
-
-  const editBook = (index) => {
-  const bookToEdit = books[index];
-  document.getElementById("bookName").value = bookToEdit.name;
-  document.getElementById("authorName").value = bookToEdit.author;
-  document.getElementById("status").value = bookToEdit.bookStatus;
-  document.getElementById("publishDate").value = bookToEdit.date;
-  document.getElementById("pages").value = bookToEdit.bookPages;
-  document.getElementById("bookLanguage").value = bookToEdit.language;
-  isEdit = true;
-  editBooksObj = bookToEdit;
-};
-
-const deleteBook = (index) => {
-  books.splice(index, 1);
-  showInfo(books);
-};
-
 
   return (
     <>
-        <div className="d-flex container justify-content-between py-4">
-          <div>
-            <input
-              style={{ width: "600px", border: "2px solid grey" }}
-              className="d-block text-light rounded-2 p-2 bg-transparent"
-              type="text"
-              placeholder="Enter a book name"
-              id="bookName"
-            />
-            <input
-              style={{ width: "600px", border: "2px solid grey" }}
-              className="d-block text-light my-4 rounded-2 p-2 bg-transparent"
-              type="text"
-              placeholder="Enter the name of author"
-              id="authorName"
-            />
-            <input
-              style={{ width: "600px", border: "2px solid grey" }}
-              className="d-block text-light my-4 rounded-2 p-2 bg-transparent"
-              type="number"
-              placeholder="Enter the year of publication"
-              id="publishDate"
-            />
-            <input
-              style={{ width: "600px", border: "2px solid grey" }}
-              className="d-block text-light my-4 rounded-2 p-2 bg-transparent"
-              type="text"
-              placeholder="Enter the status of book"
-              id="status"
-            />
-            <input
-              style={{ width: "600px", border: "2px solid grey" }}
-              className="d-block text-light my-4 rounded-2 p-2 bg-transparent"
-              type="number"
-              placeholder="Enter the pages you wanna read"
-              id="pages"
-            />
-            <input
-              style={{ width: "600px", border: "2px solid grey" }}
-              className="d-block text-light my-4 rounded-2 p-2 bg-transparent"
-              placeholder="Enter a language you wanna read"
-              type="text"
-              id="bookLanguage"
-            />
-          </div>
-          <div>
-            <input
-              style={{ width: "400px", border: "2px solid grey" }}
-              className="d-block text-light rounded-2 p-2 bg-transparent"
-              type="text"
-              onInput={searchBook}
-              placeholder="Search books"
-              id="searchBook"
-            />
-            <div className="my-books overflow-scroll">
-            <span id="booksList" className="text-light mx-2"></span>
-          </div>
+      <div className="d-flex container justify-content-between py-4">
+        <div>
+          <input
+            style={{ width: "600px", border: "2px solid grey" }}
+            className="d-block text-light rounded-2 p-2 bg-transparent"
+            type="text"
+            placeholder="Enter a book name"
+            id="bookName"
+            value={bookName}
+            onChange={(e) => {
+              setBookName(e.target.value);
+            }}
+          />
+          <input
+            style={{ width: "600px", border: "2px solid grey" }}
+            className="d-block text-light my-4 rounded-2 p-2 bg-transparent"
+            type="text"
+            placeholder="Enter the name of author"
+            id="authorName"
+            value={authorName}
+            onChange={(e) => {
+              setAuthorName(e.target.value);
+            }}
+          />
+          <input
+            style={{ width: "600px", border: "2px solid grey" }}
+            className="d-block text-light my-4 rounded-2 p-2 bg-transparent"
+            type="number"
+            placeholder="Enter the year of publication"
+            id="publishDate"
+            value={publishDate}
+            onChange={(e) => {
+              setPublishDate(e.target.value);
+            }}
+          />
+          <input
+            style={{ width: "600px", border: "2px solid grey" }}
+            className="d-block text-light my-4 rounded-2 p-2 bg-transparent"
+            type="text"
+            placeholder="Enter the status of book"
+            id="status"
+            value={status}
+            onChange={(e) => {
+              setStatus(e.target.value);
+            }}
+          />
+          <input
+            style={{ width: "600px", border: "2px solid grey" }}
+            className="d-block text-light my-4 rounded-2 p-2 bg-transparent"
+            type="number"
+            placeholder="Enter the pages you wanna read"
+            id="pages"
+            value={bookPages}
+            onChange={(e) => {
+              setBookPages(e.target.value);
+            }}
+          />
+          <input
+            style={{ width: "600px", border: "2px solid grey" }}
+            className="d-block text-light my-4 rounded-2 p-2 bg-transparent"
+            placeholder="Enter a language you wanna read"
+            type="text"
+            id="bookLanguage"
+            value={bookLanguage}
+            onChange={(e) => {
+              setBookLanguage(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          {/* <input
+            style={{ width: "400px", border: "2px solid grey" }}
+            className="d-block text-light rounded-2 p-2 bg-transparent"
+            type="text"
+            onInput={searchBook}
+            placeholder="Search books"
+            id="searchBook"
+          /> */}
+          <div
+            style={{ maxHeight: "328px" }}
+            className="my-books overflow-scroll"
+          >
+            <span className="text-light mx-2">
+              {books.map((book, index) => {
+                return (
+                  <div key={index}>
+                    <h1>{book.name}</h1>
+                    <p>{book.author}</p>
+                    <p>{book.status}</p>
+                    <p>{book.publishDate}</p>
+                    <p>{book.pages}</p>
+                    <p>{book.language}</p>
+                    <button
+                      onClick={() =>
+                        editBook(book)
+                      }
+                      className="mx-2 btn btn-info"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => deleteBook(index)}
+                      className="mx-2 btn btn-danger">
+                      Delete
+                    </button>
+                  </div>
+                );
+              })}
+            </span>
           </div>
         </div>
+      </div>
       <div className="container">
-        <button
-          onClick={onSubmit}
-          id="submitBtn"
-          className="btn w-100 my-2 btn-outline-light"
-        >
+        <button onClick={onSubmit} className="btn w-100 my-2 btn-outline-light">
           Submit
         </button>
       </div>
